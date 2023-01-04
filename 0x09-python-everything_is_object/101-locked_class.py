143a99c3dd7b0232aeb16d7fcd9ab383b97eacf4
Tamuka Lot <mnangagwatamuka@gmail.com>
2023-01-04 16:59:39 +0000

    all tasks

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """
-This is a module that containts a clas that avoids
-dynmaically created attributes
+This is a module that containts a class that avoids
+dynmically created attributes
 """
-
 
 class LockedClass:
     __slots__ = ['first_name']
