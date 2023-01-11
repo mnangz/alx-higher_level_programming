d7523550ba3c84169ce136c87d50b6a51d3375e0
Tamuka Lot <mnangagwatamuka@gmail.com>
2023-01-11 13:17:38 -0800

    0-lookup.py module documentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" Module that returns the list of available attributes and methods
+"""
+
+
 def lookup(obj):
     """ Function that returns the list of available attributes
         and methods of an object
