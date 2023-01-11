3910e319ed974aa7b2976140055d1cb618d3233f
Tamuka Lot <mnangagwatamuka@gmail.com>
2023-01-11 14:09:34 -0800

    edit module documentation

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Module documentation                                                                                                                      
+""" Module documentation
 """
 Rectangle = __import__('9-rectangle').Rectangle
 
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Module documentation                                                                                                                      
+""" Module documentation
 """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
