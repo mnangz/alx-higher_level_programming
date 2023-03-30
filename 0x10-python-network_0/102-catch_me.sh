6d3237ea87c16b00a5e3a5291f136b30b79d3b27
Tamuka Lot <mnangagwatamuka@gmail.com>
2023-03-30 09:21:26 -0700

    edits

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script that makes a request to causes an specific response
-curl -sL 0.0.0.0:5000/catch_me -X PUT -H "Origin:You got me!"
+curl -sL 0.0.0.0:5000/catch_me -X PUT -d "You got me!"
 
